9567f9aa2d955d5938b1a1b413671cae0c875d12
Luis Tobon <luisandrestobon@gmail.com>
2021-07-23 22:12:41 -0500

    Add Author and Book entities

--- src/main/java/com/luistobon/spring5webapp/domain/Author.java
+++ src/main/java/com/luistobon/spring5webapp/domain/Author.java
@@ -2,19 +2,40 @@ package com.luistobon.spring5webapp.domain;
 
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Author {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
 	private String firstName;
 	private String lastName;
+	
+	@ManyToMany(mappedBy = "authors")
 	private Set<Book> books;
 	
 	public Author() {
 	}
 	
 	public Author(String firstName, String lastName, Set<Book> books) {
-		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.books = books;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getFirstName() {
